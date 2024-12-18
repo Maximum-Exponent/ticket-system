ad72cce827082ce9a0de9d1ca9b2b4c965308a1f
RiccardoCipriani <riccardo.cipriani.crescio@gmail.com>
2024-12-18 15:20:42 +0100

    ticketEditor fatto, ora tocca al fotum

---
+++ index/static/assets/js/ticketEditor.js
@@ -0,0 +1,30 @@
+const maxChars = 2000;
+    
+    function execCmd(command, value = null) {
+        document.execCommand(command, false, value);
+    }
+
+    function updateCharacterCount(editor) {
+        const text = editor.innerText || editor.textContent;
+        const remainingChars = maxChars - text.length;
+        const charCountElement = document.getElementById('charCount');
+
+        if (remainingChars >= 0) {
+            charCountElement.textContent = remainingChars;
+        } else {
+            editor.innerText = text.substring(0, maxChars);
+            charCountElement.textContent = 0;
+        }
+    }
+
+    function limitCharacters(event) {
+        const editor = document.querySelector('.editor');
+        const text = editor.innerText || editor.textContent;
+        if (text.length >= maxChars && event.key !== 'Backspace' && event.key !== 'Delete') {
+            event.preventDefault();
+        }
+    }
+
+const sendTicketButton = document.getElementById('sendTicketButton');
+const errorMessage = document.getElementById('error-message');
+const messagePlaceholder = document.getElementById('message-placeholder');
